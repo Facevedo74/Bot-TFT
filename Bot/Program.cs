d212d78c7c0c2f5d63181f64192bb299036ff160
Facevedo74 <gafacevedo1@gmail.com>
2023-10-15 12:15:21 -0500

    Normalize version bot

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -52,8 +52,8 @@ namespace Bot
                     if (matchings[0].Similarity > 0.94f)
                     {
                         Console.WriteLine(fecha + " Buscando Partida");
-                        Cursor.Position = new Point(850, 925);
-                        auto.MouseClick("LEFT", 850, 925, 1, -1);
+                        Cursor.Position = new Point(584, 691);
+                        auto.MouseClick("LEFT", 584, 691, 1, -1);
                         Thread.Sleep(2000);
                     }
                     else
@@ -63,8 +63,8 @@ namespace Bot
                         if (matchings[0].Similarity > 0.95f)
                         {
                             Console.WriteLine(fecha + " Partida Encontrada");
-                            Cursor.Position = new Point(958, 774);
-                            auto.MouseClick("LEFT", 958, 774, 1, -1);
+                            Cursor.Position = new Point(683, 560);
+                            auto.MouseClick("LEFT", 683, 560, 1, -1);
                         }
                         else
                         {
@@ -80,7 +80,7 @@ namespace Bot
                                 //Caminamos
                                 fecha = DateTime.Now.ToString("hh:mm:ss");
                                 Console.WriteLine(fecha + " Eligiendo campeon");
-                                auto.MouseClick("RIGHT", 960, 350, 1, -1);
+                                auto.MouseClick("RIGHT", 686, 222, 1, -1);
                                 //Esperamos para comprar campeones
                                 Thread.Sleep(60000);
                                 //Compramos 2 Campeones
@@ -88,60 +88,60 @@ namespace Bot
                                 Console.WriteLine(fecha + " Comprando Campeones");
                                 for (int i = 0; i < 6; i++)
                                 {
-                                    auto.MouseClick("LEFT", 560, 1000, 1, -1);
+                                    auto.MouseClick("LEFT", 434, 666, 1, -1);
                                     Thread.Sleep(100);
                                 }
                                 Thread.Sleep(1000);
                                 for (int i = 0; i < 6; i++)
                                 {
-                                    auto.MouseClick("LEFT", 780, 1000, 1, -1);
+                                    auto.MouseClick("LEFT", 562, 666, 1, -1);
                                     Thread.Sleep(100);
                                 }                               
                                 Thread.Sleep(60000);
 
                                 //Subimos de Nivel
-                                for (int h=0; h<10; h++) {
+                                for (int h=0; h<12; h++) {
                                 //for (int h=0; h<14; h++) { 
                                 fecha = DateTime.Now.ToString("hh:mm:ss");
                                 Console.WriteLine(fecha + " Subiendo de Nivel, Iteracion:" + h);                               
                                 for (int i = 0; i < 2; i++)
                                 {
-                                    auto.MouseClick("LEFT", 360, 960, 1, -1);
+                                    auto.MouseClick("LEFT", 300, 700, 1, -1);
                                     Thread.Sleep(600);
                                 }
                                 //Compramos 4 Campeones mas
                                 Console.WriteLine(fecha + " Comprando Campeones");
                                 for (int i = 0; i < 6; i++)
                                 {
-                                    auto.MouseClick("LEFT", 560, 1000, 1, -1);
+                                    auto.MouseClick("LEFT", 434, 666, 1, -1);
                                     Thread.Sleep(100);
                                 }
                                 Thread.Sleep(1100);
                                 for (int i = 0; i < 7; i++)
                                 {
-                                    auto.MouseClick("LEFT", 780, 1000, 1, -1);
+                                    auto.MouseClick("LEFT", 562, 666, 1, -1);
                                     Thread.Sleep(100);
                                 }
                                 Thread.Sleep(1100);
                                 for (int i = 0; i < 7; i++)
                                 {
-                                    auto.MouseClick("LEFT", 960, 1000, 1, -1);
+                                    auto.MouseClick("LEFT", 696, 666, 1, -1);
                                     Thread.Sleep(100);
                                 }
                                 Thread.Sleep(1100);
                                 for (int i = 0; i < 7; i++)
                                 {
-                                    auto.MouseClick("LEFT", 1160, 1000, 1, -1);
+                                    auto.MouseClick("LEFT", 830, 666, 1, -1);
                                     Thread.Sleep(100);
                                 }                               
                                 Thread.Sleep(1000);
                                 //Caminamos por el Mapa
                                 Console.WriteLine(fecha + " Caminando");
-                                auto.MouseClick("RIGHT", 1280, 600, 1, -1);
+                                auto.MouseClick("RIGHT", 864, 439, 1, -1);
                                 Thread.Sleep(1000);
-                                auto.MouseClick("RIGHT", 965, 274, 1, -1);
+                                auto.MouseClick("RIGHT", 467, 274, 1, -1);
                                 Thread.Sleep(1000);
-                                auto.MouseClick("RIGHT", 1300, 500, 1, -1);
+                                auto.MouseClick("RIGHT", 897, 241, 1, -1);
                                 fecha = DateTime.Now.ToString("hh:mm:ss");
                                 //Esperar Minuto 10
                                 fecha = DateTime.Now.ToString("hh:mm:ss");
@@ -150,27 +150,27 @@ namespace Bot
                                 Console.WriteLine(fecha + " Intentando Tirar Surrender");
                                 for (int i = 0; i < 10; i++)
                                 {
-                                  Cursor.Position = new Point(1905, 886);
-                                  auto.MouseClick("LEFT", 1905, 886, 1, -1);
+                                  Cursor.Position = new Point(1185, 598);
+                                  auto.MouseClick("LEFT", 1185, 598, 1, -1);
                                     Thread.Sleep(100);
                                 }
                                 for (int i = 0; i < 3; i++)
                                 {
-                                    auto.MouseClick("LEFT", 750, 870, 1, -1);
+                                    auto.MouseClick("LEFT", 494, 685, 1, -1);
                                 }
                                 Thread.Sleep(1000);
                                 Console.WriteLine(fecha + " Corfirmando Surrender ");
                                 for (int i = 0; i < 10; i++)
                                 {
-                                    auto.MouseClick("LEFT", 1030, 490, 1, -1);
+                                    auto.MouseClick("LEFT", 617, 428, 1, -1);
                                     Thread.Sleep(100);
                                 }
                                 Thread.Sleep(5000);
                                // auto.MouseClick("LEFT", 617, 428, 1, -1);
-                                Thread.Sleep(6000);
-                                //  auto.MouseClick("LEFT", 617, 428, 1, -1);
-                                // Thread.Sleep(6000);
-                                auto.MouseClick("LEFT", 830, 935, 1, -1);
+                               Thread.Sleep(1000);
+                              //  auto.MouseClick("LEFT", 617, 428, 1, -1);
+                               // Thread.Sleep(6000);
+                                auto.MouseClick("LEFT", 575, 691, 1, -1);
                             }
                             else
                             {
@@ -178,23 +178,19 @@ namespace Bot
                                 matchings = tm.ProcessImage(Rendirse, Captura);
                                 if (matchings[0].Similarity > 0.95f)
                                 {
-                                    for (int i = 0; i < 3; i++)
-                                    {
-                                        auto.MouseClick("LEFT", 750, 870, 1, -1);
-                                    }
-                                    Thread.Sleep(1000);
+                                    auto.MouseClick("LEFT", 494, 685, 1, -1);
                                     Console.WriteLine(fecha + " Corfirmando Surrender ");
                                     for (int i = 0; i < 10; i++)
                                     {
-                                        auto.MouseClick("LEFT", 1030, 490, 1, -1);
+                                        auto.MouseClick("LEFT", 617, 428, 1, -1);
                                         Thread.Sleep(100);
                                     }
-                                    //Thread.Sleep(5000);
-                                    //auto.MouseClick("LEFT", 617, 428, 1, -1);
-                                    //Thread.Sleep(1000);
-                                    //auto.MouseClick("LEFT", 617, 428, 1, -1);
+                                    Thread.Sleep(5000);
+                                    auto.MouseClick("LEFT", 617, 428, 1, -1);
+                                    Thread.Sleep(1000);
+                                    auto.MouseClick("LEFT", 617, 428, 1, -1);
                                     Thread.Sleep(6000);
-                                    auto.MouseClick("LEFT", 830, 935, 1, -1);
+                                    auto.MouseClick("LEFT", 575, 691, 1, -1);
                                 }
                                 else
                                 {
@@ -205,16 +201,15 @@ namespace Bot
                                         Console.WriteLine(fecha + " Corfirmando Surrender ");
                                         for (int i = 0; i < 10; i++)
                                         {
-                                            auto.MouseClick("LEFT", 1030, 490, 1, -1);
+                                            auto.MouseClick("LEFT", 617, 428, 1, -1);
                                             Thread.Sleep(100);
                                         }
-                                        //Thread.Sleep(5000);
-                                        //auto.MouseClick("LEFT", 617, 428, 1, -1);
-                                        //Thread.Sleep(1000);
-                                        //auto.MouseClick("LEFT", 617, 428, 1, -1);
+                                        Thread.Sleep(5000);
+                                        auto.MouseClick("LEFT", 617, 428, 1, -1);
+                                        Thread.Sleep(1000);
+                                        auto.MouseClick("LEFT", 617, 428, 1, -1);
                                         Thread.Sleep(6000);
-                                        auto.MouseClick("LEFT", 830, 935, 1, -1);
-
+                                        auto.MouseClick("LEFT", 575, 691, 1, -1);
                                     }
                                     else
                                     {
@@ -227,23 +222,23 @@ namespace Bot
                                             Console.WriteLine(fecha +" VolverAJugar ");
                                             //auto.MouseClick("LEFT", 567, 371, 1, -1);
                                             //Thread.Sleep(1000);
-                                            Cursor.Position = new Point(830, 935);
-                                            auto.MouseClick("LEFT", 830, 935, 1, -1);
+                                            Cursor.Position = new Point(575, 691);
+                                            auto.MouseClick("LEFT", 575, 691, 1, -1);
 
                                         }
                                         else
                                         {
                                             //En caso de ser eliminados sin poder tirar surrender, precionar boton salir
                                             matchings = tm.ProcessImage(Salir, Captura);
-                                            if (matchings[0].Similarity > 0.90f)
+                                            if (matchings[0].Similarity > 0.91f)
                                             {
                                                 Console.WriteLine(fecha + " Salir");
-                                                Cursor.Position = new Point(840, 525);
-                                                auto.MouseClick("LEFT", 840, 525, 1, -1);
+                                                Cursor.Position = new Point(600, 371);
+                                                auto.MouseClick("LEFT", 600, 371, 1, -1);
                                                 Thread.Sleep(300);
-                                                auto.MouseClick("LEFT", 840, 525, 1, -1);
+                                                auto.MouseClick("LEFT", 600, 371, 1, -1);
                                                 Thread.Sleep(300);
-                                                auto.MouseClick("LEFT", 840, 525, 1, -1);
+                                                auto.MouseClick("LEFT", 600, 371, 1, -1);
                                             }
                                             else
                                             {
@@ -251,26 +246,13 @@ namespace Bot
                                                 matchings = tm.ProcessImage(ReintentandoSurrender, Captura);
                                                 if (matchings[0].Similarity > 0.98f)
                                                 {
-                                                    Console.WriteLine(fecha + " Intentando Tirar Surrender");
+                                                    Console.WriteLine(fecha + " Reintentando Surrender");
                                                     for (int i = 0; i < 10; i++)
                                                     {
-                                                        Cursor.Position = new Point(1905, 886);
-                                                        auto.MouseClick("LEFT", 1905, 886, 1, -1);
+                                                        Cursor.Position = new Point(1185, 598);
+                                                        auto.MouseClick("LEFT", 1185, 598, 1, -1);
                                                         Thread.Sleep(100);
                                                     }
-                                                    for (int i = 0; i < 3; i++)
-                                                    {
-                                                        auto.MouseClick("LEFT", 750, 870, 1, -1);
-                                                    }
-                                                    Thread.Sleep(1000);
-                                                    Console.WriteLine(fecha + " Corfirmando Surrender ");
-                                                    for (int i = 0; i < 10; i++)
-                                                    {
-                                                        auto.MouseClick("LEFT", 1030, 490, 1, -1);
-                                                        Thread.Sleep(100);
-                                                    }
-                                                    Thread.Sleep(1000);
-
                                                 }
                                                 else
                                                 {
@@ -320,7 +302,6 @@ namespace Bot
                 {
                     Console.WriteLine(fecha + " Hola Soy un error :'v o eso me decia mi mama :c");
                     Console.WriteLine(fecha + " Fuera bromas tu resolucion no es la correcta lee las instrucciones");
-                    Console.WriteLine(exception);
                 }
                 //Thread.Sleep(1500);
             }
